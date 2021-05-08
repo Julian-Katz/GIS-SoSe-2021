4ec0e26619417f413ef3716d01677cb0c46db8c5
juliankatz@gmx.de <juliankatz>
2021-05-08 23:56:30 +0200

    data

--- Aufgabe2.3/script.js
+++ Aufgabe2.3/script.js
@@ -17,6 +17,25 @@ newRecBtn.addEventListener("click", function () {
     console.log(randomDiv);
 });
 // Ende - Aufgabe 1
+// Load dynamik content
+window.addEventListener("load", function () {
+    insertOptionsEis();
+    console.log("Dom load Event ausgeführt");
+});
+// Ende - Load dynamik content
+// Eis Options laden
+let optionsEis = document.getElementById("optionsEis");
+function insertOptionsEis() {
+    angebotWaffeln.forEach(element => {
+        let domItem = document.createElement("div");
+        domItem.classList.add("btn", "options");
+        domItem.innerText = `${element[0]} - ${element.größe} - ${element.preis}€`;
+        console.log(element);
+        console.log(domItem);
+        optionsEis.appendChild(domItem);
+    });
+}
+// Ende - Eis Options laden
 // Eis zusammenstellen
 let eis1 = { waffel: angebotWaffeln[1], eiskugeln: [angebotEisskugeln[1]], toppings: [angebotToppings[1]], preis: 2 };
 console.log(eis1);
--- Aufgabe2.3/script.ts
+++ Aufgabe2.3/script.ts
@@ -20,8 +20,31 @@ newRecBtn.addEventListener("click", function (): void {
 
 });
 // Ende - Aufgabe 1
+// Load dynamik content
+window.addEventListener("load", function (): void {
+    insertOptionsEis();
+    console.log("Dom load Event ausgeführt");
+
+})
+// Ende - Load dynamik content
+
+// Eis Options laden
+let optionsEis: HTMLElement = document.getElementById("optionsEis");
+
+function insertOptionsEis(): void {
+    angebotWaffeln.forEach(element => {
+        let domItem: HTMLElement = document.createElement("div");
+        domItem.classList.add("btn", "options");
+        domItem.innerText = `${element[0]} - ${element.größe} - ${element.preis}€`;
+        console.log(element);
+        console.log(domItem);
+        optionsEis.appendChild(domItem);
+    });
+}
+// Ende - Eis Options laden
 
 // Eis zusammenstellen
 let eis1: EisKonfiguration = { waffel: angebotWaffeln[1], eiskugeln: [angebotEisskugeln[1]], toppings: [angebotToppings[1]], preis: 2 };
 console.log(eis1);
 // Ende - Eis zusammenstellen
+
