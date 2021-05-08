ec5dad818e31633bf83a3c15adfb45cc25116608
juliankatz@gmx.de <juliankatz>
2021-05-08 20:29:20 +0200

    classen

---
+++ Aufgabe2.3/script.ts
@@ -0,0 +1,66 @@
+import { Daten } from "./data"
+namespace Aufgabe2_3 {
+    let newRecBtn: HTMLElement = document.getElementById("newRec");
+    let resetRec: HTMLElement = document.getElementById("resetRecs");
+
+    let recFrame: HTMLElement = document.getElementById("recFrame");
+
+    resetRec.addEventListener("click", function (): void {
+        recFrame.innerHTML = ``;
+    });
+
+    newRecBtn.addEventListener("click", function (): void {
+        let randomDiv: HTMLElement = document.createElement("div");
+        randomDiv.style.top = `${Math.random() * 100}%`;
+        randomDiv.style.left = `${Math.random() * 100}%`;
+        randomDiv.style.height = `${Math.random() * 100}%`;
+        randomDiv.style.width = `${Math.random() * 100}%`;
+        randomDiv.style.background = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        randomDiv.classList.add("randomRec");
+        recFrame.appendChild(randomDiv);
+        console.log(randomDiv);
+
+    });
+
+    interface EisKonfiguration {
+        waffel: Waffel;
+        eiskugeln: Eiskugel[];
+        toppings: Topping[];
+        preis: number;
+    }
+
+    class Waffel {
+        art: String;
+        größe: String;
+        preis: number;
+
+        constructor(_art: String, _größe: String, _preis: number) {
+            this.art = _art;
+            this.größe = _größe;
+            this.preis = _preis;
+        }
+    }
+
+    class Eiskugel {
+        geschmack: String;
+        preis: number;
+
+        constructor(_geschmack: String) {
+            this.geschmack = _geschmack;
+            this.preis = 1.80;
+        }
+    }
+
+    class Topping {
+        art: String;
+        preis: number;
+
+        constructor(_art: String, _preis: number) {
+            this.art = _art;
+            this.preis = _preis;
+        }
+    }
+
+    let eis1: EisKonfiguration = { waffel: Daten.angebotWaffeln[1], eiskugeln: [Daten.angebotEisskugeln[1]], toppings: [Daten.angebotToppings[1]], preis: 2 };
+    console.log(eis1);
+}
