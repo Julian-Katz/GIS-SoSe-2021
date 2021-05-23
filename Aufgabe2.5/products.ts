11818079bf9b02cbc1aa5f16776880e15704f3b2
juliankatz@gmx.de <juliankatz>
2021-05-23 12:23:54 +0200

    ,l

---
+++ Aufgabe2.5/products.ts
@@ -0,0 +1,27 @@
+namespace data {
+    // Eis Datenstruktur
+    interface EisKonfiguration {
+        waffel: Waffel;
+        eiskugeln: Eiskugel[];
+        toppings: Topping[];
+        preis: number;
+    }
+
+    interface Waffel {
+        art: String;
+        größe: String;
+        preis: number;
+    }
+
+    interface Eiskugel {
+        geschmack: String;
+        preis: number;
+    }
+
+    interface Topping {
+        art: String;
+        preis: number;
+    }
+    // Ende - Eis Datenstruktur
+
+}
