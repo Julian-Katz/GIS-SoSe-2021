8573350930b31d975077c3cefa9efa295e68c036
Julian Katz <juliankatz@gmx.de>
2021-06-06 12:20:19 +0200

    fix

--- Aufgabe3.2/local/script.ts
+++ Aufgabe3.2/local/script.ts
@@ -4,7 +4,7 @@ namespace Aufgabe3_2 {
     form.addEventListener("submit", handleFormSubmit);
     form2.addEventListener("submit", handleFormSubmit2);
 
-    async function handleFormSubmit(_event: Event): Promise<any> {
+    async function handleFormSubmit(_event: Event): Promise<void> {
         _event.preventDefault();
         let url: string = "https://testgisjk.herokuapp.com/";
         // let url: string = "http://localhost:8100/";
@@ -21,7 +21,7 @@ namespace Aufgabe3_2 {
         body.appendChild(htmlElment);
 
     }
-    async function handleFormSubmit2(_event: Event): Promise<any> {
+    async function handleFormSubmit2(_event: Event): Promise<void> {
         _event.preventDefault();
         // let url: string = "https://testgisjk.herokuapp.com/";
         let url: string = "http://localhost:8100/";
